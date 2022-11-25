99494964bac88b8011e091ee19d2c11532894508
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2022-11-25 12:22:18 +0530

    React tutorial [Codevolution]

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,1 +1,30 @@
-console.log("from index.js");
+const redux = require("redux");
+const createStore = redux.createStore;
+
+const BUY_CAKE = "BUY_CAKE";
+
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "First Redux Action",
+  };
+}
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+console.log("Initial State", store.getState());
