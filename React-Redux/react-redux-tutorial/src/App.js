98120c76f53dcfbde76b4824b45f8f0c599568e0
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2022-11-28 00:20:05 +0530

    React tutorial [Codevolution]

--- React-Redux/react-redux-tutorial/src/App.js
+++ React-Redux/react-redux-tutorial/src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { Provider } from "react-redux";
 import CakeContainer from "./Components/CakeContainer";
 import store from "./redux/store";
+import HooksCakeContainer from "./Components/HooksCakeContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <CakeContainer />
+        <HooksCakeContainer />
       </div>
     </Provider>
   );
